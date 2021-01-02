de54f3ff1c36d014346474ab8f6912bcc78b47d8
Stephen Proulx <stephen.proulx@gmail.com>
2021-01-02 09:57:36 -0800

    replace shiny app code

--- Temp/app.R
+++ Temp/app.R
@@ -1,104 +1,77 @@
-# A little shiny app to estimate the current prevalence of a disease including false positive rates
-# Written by Stephen Proulx sproul@ucsb.edu
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
 library(shiny)
 
 library(tidyverse)
-library(rstan)
-options(mc.cores = parallel::detectCores())
-library(bayesplot)
-
-
-prevalence_model <- readRDS("model.rds") 
 
 
 
-# Define UI
+# In this problem, we assume that there is a probability that you have cold
+# symptoms when you don't have covid, for example because you have a cold. 
+# There is some fraction of the population who has covid, and given that an 
+# individual has covid, they have a chance of having cold symptoms
+
+# fixed parameters for the example
+probSgC <- 0.25 # probability of symptoms given covid
+
+
+# Prepare our dataframes 
+
+
+params <- tibble(
+    probSgNC   = seq(from=0, to=1,by=0.01),
+    incidence   = seq(from=0, to=1,by=0.01)
+    )
+
+# All possible combinations of false positive and incidence rate.
+params <- params %>% expand(probSgNC, incidence) %>%
+    mutate(probCgS = probSgC * incidence /(probSgC * incidence + probSgNC * (1-incidence)  ) ,
+           info = log(probCgS/incidence))
+           
+
+
+    
+
+# Define UI for application that draws a histogram
 ui <- fluidPage(
-    
+
     # Application title
-    titlePanel("Estimate Prevalence"),
-    
-    
+    titlePanel("Inferring disease state from individual testing data"),
+
+    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("n_pre",
-                        "Number of samples in pre-disease test set",
-                        min = 0,
-                        max = 1000,
-                        value = 401),
-            sliderInput("pos_pre",
-                        "Number of samples that tested positive in pre-disease test set",
-                        min = 0,
-                        max = 1000,
-                        value = 2),
-            sliderInput("n_sample",
-                        "Number of samples in active epidemic test set",
-                        min = 1,
-                        max = 10000,
-                        value = 3330),
-            sliderInput("pos_pos",
-                        "Number of samples that tested positive in active epidemic test set",
-                        min = 1,
-                        max = 10000,
-                        value = 50),
-            # sliderInput("lbound",
-            #             "lower bound on prior for prevalence",
-            #             min = 0,
-            #             max = 1,
-            #             value = 0),
-            # sliderInput("ubound",
-            #             "upper bound on prior for prevalence",
-            #             min = 0,
-            #             max = 1,
-            #             value = 1),
-            actionButton("do", "Run Models")
+            sliderInput("prevalence",
+                        "Disease prevalence in the population:",
+                        min = 0.01,
+                        max = 0.99,
+                        value = 0.5)
         ),
-        
+
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("results")
+           plotOutput("probPlot")
         )
     )
 )
 
-
-
-# Define server logic 
+# Define server logic required to draw the plot
 server <- function(input, output) {
-    
-    observeEvent(input$do, {
-        
-        
-        
-        stan_data <-   list( n_pre = input$n_pre , pos_pre = input$pos_pre , 
-                             n_sample=input$n_sample , pos_pos = input$pos_pos ,
-                             prior_prev = c(0,1))
-        
-        stanfit_bin_antibody <- sampling(prevalence_model, data = stan_data, chains = 2,
-                                     iter = 1000, seed = 213123)
-        
-        
-        post <- posterior_samples(stanfit_bin_antibody)
-        
-        output$results <-  renderPlot({
-            mcmc_areas(post,area_method = "equal height" , pars=c("mu"),prob=.95)+
-                scale_x_continuous(limits = c(0,input$pos_pos/input$n_sample) )+
-                labs(title = "Bounds of estimate prevelance",
-                     x = "Disease Prevalence", y = "Posterior Probability Density") +
-                theme_bw()   
-        })
-        
+
+    output$probPlot <- renderPlot({ggplot(data=filter(params,incidence==input$prevalence),
+                                          aes(x=probSgNC, y = probCgS)) + 
+            geom_line()+
+            scale_y_continuous( limits=c(0,1))+
+            labs( x="False Positive Rate" , y="Probability of covid given symptoms")
         
     })
-    
-    
-    
-   
-    
-    
-    
-    
 }
 
 # Run the application 
